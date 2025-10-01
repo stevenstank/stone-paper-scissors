8b57750a1926ef5082b363b6e1ec8ea57765ffaf
Saksham Khandelwal <224991604+stevenstank@users.noreply.github.com>
2025-10-02 00:02:31 +0530

    Add files via upload

---
+++ New folder (2)/script.js
@@ -0,0 +1,35 @@
+const prompt = require('prompt-sync')();
+let playerChoice = prompt("Enter Rock, Paper, or Scissors:").toLowerCase().trim()
+const choice = ["rock", "paper", "scissors"]
+if (choice.includes(playerChoice)) {
+    console.log("You chose", playerChoice)
+
+}
+else {
+    console.log("Invalid Choice")
+    return
+}
+const computerChoice = choice[Math.floor(Math.random() * 3)]
+console.log("Computer chose", computerChoice)
+if (computerChoice == playerChoice) {
+    console.log("Its a tie!")
+}
+else if (computerChoice == "rock" && playerChoice == "scissors") {
+    console.log("Computer Wins!")
+
+}
+else if (computerChoice == "paper" && playerChoice == "rock") {
+    console.log("Computer Wins!")
+
+}
+else if (computerChoice == "scissors" && playerChoice=="paper")
+{
+    console.log("Computer Wins!")
+
+}
+else{
+    console.log("You Win!")
+}
+
+
+
